5bd4cc96301f91c5b47496342010f9766a424d8f
KishalJayalath <kishalsj2002@gmail.com>
2025-04-17 09:53:14 +0530

    front-end and back-end

--- components/common/Loader.js
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="overlay flex justify-center items-center bg-slate-700 bg-opacity-50">
-      <div className="load"> </div>
-    </div>
-  );
-}
